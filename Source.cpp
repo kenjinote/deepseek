dae5ffbf7ad1627afa366756c91ba371de482538
kenjinote <2605401+kenjinote@users.noreply.github.com>
2025-02-01 01:22:34 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,140 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#pragma comment(lib, "wininet")
+
+#include <windows.h>
+#include <wininet.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LPBYTE sendDeepSeek(LPWSTR lpszText)
+{
+	LPBYTE lpszReturn = 0;
+	DWORD dwSize = 0;
+	const HINTERNET hSession = InternetOpen(TEXT("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"), INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, INTERNET_FLAG_NO_COOKIES);
+	if (hSession)
+	{
+		URL_COMPONENTS uc = { 0 };
+		TCHAR HostName[MAX_PATH];
+		TCHAR UrlPath[MAX_PATH];
+		uc.dwStructSize = sizeof(uc);
+		uc.lpszHostName = HostName;
+		uc.lpszUrlPath = UrlPath;
+		uc.dwHostNameLength = MAX_PATH;
+		uc.dwUrlPathLength = MAX_PATH;
+		InternetCrackUrl(L"https://api.deepseek.ai/v1/query", 0, 0, &uc);
+		const HINTERNET hConnection = InternetConnect(hSession, HostName, INTERNET_DEFAULT_HTTP_PORT, 0, 0, INTERNET_SERVICE_HTTP, 0, 0);
+		if (hConnection)
+		{
+			const HINTERNET hRequest = HttpOpenRequest(hConnection, TEXT("GET"), UrlPath, 0, 0, 0, 0, 0);
+			if (hRequest)
+			{
+				TCHAR szReferer[1024];
+				lstrcpy(szReferer, TEXT("Content-Type: application/json\r\nAuthorization:"));
+				lstrcat(szReferer, L"");
+				HttpAddRequestHeaders(hRequest, szReferer, lstrlen(szReferer), HTTP_ADDREQ_FLAG_REPLACE | HTTP_ADDREQ_FLAG_ADD);
+				HttpSendRequest(hRequest, 0, 0, 0, 0);
+				lpszReturn = (LPBYTE)GlobalAlloc(GMEM_FIXED, 1);
+				DWORD dwRead;
+				static BYTE szBuf[1024 * 4];
+				LPBYTE lpTmp;
+				for (;;)
+				{
+					if (!InternetReadFile(hRequest, szBuf, (DWORD)sizeof(szBuf), &dwRead) || !dwRead) break;
+					lpTmp = (LPBYTE)GlobalReAlloc(lpszReturn, (SIZE_T)(dwSize + dwRead), GMEM_MOVEABLE);
+					if (lpTmp == NULL) break;
+					lpszReturn = lpTmp;
+					CopyMemory(lpszReturn + dwSize, szBuf, dwRead);
+					dwSize += dwRead;
+				}
+
+				lpszReturn[dwSize] = 0;
+
+				InternetCloseHandle(hRequest);
+			}
+			InternetCloseHandle(hConnection);
+		}
+		InternetCloseHandle(hSession);
+	}
+	return lpszReturn;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hButton;
+	static HWND hEdit1;
+	static HWND hEdit2;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hEdit1 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("変換"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		hEdit2 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("EDIT"), 0, WS_VISIBLE | WS_CHILD | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_READONLY, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		break;
+	case WM_SIZE:
+		MoveWindow(hEdit1, 0, 0, LOWORD(lParam) - 128, HIWORD(lParam) / 2, TRUE);
+		MoveWindow(hButton, LOWORD(lParam) - 128, 0, 128, 32, TRUE);
+		MoveWindow(hEdit2, 0, HIWORD(lParam) / 2, LOWORD(lParam) - 128, HIWORD(lParam) / 2, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			DWORD dwSize = GetWindowTextLength(hEdit1) + 1;
+			LPCWSTR lpszText = (LPCWSTR)GlobalAlloc(GPTR, dwSize * sizeof(WCHAR));
+			GetWindowText(hEdit1, (LPWSTR)lpszText, dwSize);
+			LPBYTE lpszReturn = sendDeepSeek((LPWSTR)lpszText);
+			if (lpszReturn)
+			{
+				SetWindowTextA(hEdit2, (LPSTR)lpszReturn);
+				GlobalFree((HGLOBAL)lpszReturn);
+			}
+			GlobalFree((HGLOBAL)lpszText);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("deepseek"),
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
